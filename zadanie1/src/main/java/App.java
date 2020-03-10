d08bc36dfc4a7c706afb001e6f1896d4ed6559cd
Mateusz Olsztyński <matiol1@hotmail.com>
2020-03-10 20:35:13 +0100

    Add mutate, cross operation + fix greedy alg

--- zadanie1/src/main/java/App.java
+++ zadanie1/src/main/java/App.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
-    private static String dataFile = new File("src/main/resources/berlin11_modified.tsp").getAbsolutePath();
+    private static String dataFile = new File("src/main/resources/kroA100.tsp").getAbsolutePath();
 
     public static List<Edge> loadEdges() throws Exception{
         List<Edge> edges = new ArrayList<Edge>();
@@ -27,17 +27,20 @@ public class App {
         List<Edge> edges = loadEdges();
         Edge[] edgesTab=new Edge[edges.size()];
         edges.toArray(edgesTab);
-
         GreedyAlg greedyAlg = new GreedyAlg();
         Individual ind3 = greedyAlg.solve(edgesTab);
+        ind3.showGenotype();
+        System.out.println(ind3.fitness());
 
         SalesmanProblem pro = new SalesmanProblem(edges);
+
         Individual ind = pro.generateIndividual();
         Individual ind2 = pro.generateIndividual();
 
         ind.showGenotype();
         ind2.showGenotype();
-        ind.cross(ind2);
+        System.out.println(ind.cross(ind2).fitness());
+
 
     }
 }
--- zadanie1/src/main/java/GreedyAlg.java
+++ zadanie1/src/main/java/GreedyAlg.java
@@ -1,43 +1,39 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GreedyAlg {
 
     public Individual solve(Edge[] edges){
-        List<Edge> toFind = new ArrayList<Edge>();
+        List<Integer> list= new ArrayList<Integer>();
         Edge[] result = new Edge[edges.length];
-        int bestIndex=0;
-        double distance=0;
-        double tempDistance;
-        for(int k=0;k<edges.length;k++){
-            toFind.add(edges[k]);
+        for(int k=1;k<edges.length;k++){
+            list.add(edges[k].id);
         }
         result[0] = edges[0];
-        toFind.remove(0);
-        int currentEdgeIndex;
-        int bestJ=0;
-        for(int i=1; i<edges.length;i++){
-            currentEdgeIndex = i-1;
-            for(int j=0; j<toFind.size();j++){
-                if(j == 0){
-                    bestIndex =toFind.get(0).id - 1;
-                    distance = Math.sqrt(Math.pow((double)result[currentEdgeIndex].x - (double)edges[bestIndex].x, 2) +
-                            Math.pow((double)result[currentEdgeIndex].y - (double)edges[bestIndex].y, 2));
-                    bestJ = 0;
+        int bestIndex = 0;
+        double bestDistance = 0;
+        double tempDistance = 0;
+        int position = 1;
+        while(position < edges.length){
+            for(int i=0;i<list.size();i++){
+                if(i==0){
+                    bestDistance = Math.sqrt(Math.pow((double)edges[list.get(0) - 1].x - (double)result[position-1].x, 2) +
+                                     Math.pow((double)edges[list.get(0) - 1].y - (double)result[position-1].y, 2));
+                    bestIndex = 0;
                 }else{
-                    tempDistance = Math.sqrt(Math.pow((double)result[currentEdgeIndex].x - (double)edges[toFind.get(j).id - 1].x, 2) +
-                            Math.pow((double)result[currentEdgeIndex].y - (double)edges[toFind.get(j).id - 1].y, 2));
-                    if(tempDistance<distance){
-                        bestIndex = toFind.get(j).id -1;
-                        bestJ = j;
+                    tempDistance = Math.sqrt(Math.pow((double)edges[list.get(i)-1].x - (double)result[position-1].x, 2) +
+                            Math.pow((double)edges[list.get(i)-1].y - (double)result[position-1].y, 2));
+                    if(tempDistance < bestDistance){
+                        bestDistance = tempDistance;
+                        bestIndex = i;
                     }
-
-                }
-                if(j == toFind.size() - 1){
-                    toFind.remove(bestJ);
-                    result[i] = edges[bestIndex];
                 }
             }
+            result[position] = edges[list.get(bestIndex)-1];
+            list.remove(bestIndex);
+            position++;
         }
 
         Individual ind = new Individual(edges.length);
